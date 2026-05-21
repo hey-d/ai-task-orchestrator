feff7a386db70e779e7346b641151bd9415630f9
hey-d <dushyantmanghani@gmail.com>
2026-05-21 11:28:30 +0530

    debugging 6

--- server/src/config/aiConfig.js
+++ server/src/config/aiConfig.js
@@ -1,10 +1,10 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 require("dotenv").config();
 
-const genAI = new GoogleGenerativeAI(process.env.Gemini_API_Key);
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
 const model = genAI.getGenerativeModel({
-    model: "gemini-3-flash-preview",
+  model: "gemini-3-flash-preview",
 });
 
 module.exports = model;
